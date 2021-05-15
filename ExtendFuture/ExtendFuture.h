52d43c9be1c2bd675f9b9a9e47dede8ebedcb7b9
Cao Phuoc Thanh <caophuocthanh@gmail.com>
2021-05-15 18:38:14 +0700

    Initial Commit

---
+++ ExtendFuture/ExtendFuture.h
@@ -0,0 +1,18 @@
+//
+//  ExtendFuture.h
+//  ExtendFuture
+//
+//  Created by Cao Phuoc Thanh on 15/05/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ExtendFuture.
+FOUNDATION_EXPORT double ExtendFutureVersionNumber;
+
+//! Project version string for ExtendFuture.
+FOUNDATION_EXPORT const unsigned char ExtendFutureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExtendFuture/PublicHeader.h>
+
+
